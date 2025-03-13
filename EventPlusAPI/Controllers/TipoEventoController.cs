e3be4f77e54519c0a2f090032e9ad3be63948e60
ERIK MENEGHETTI <45333900809@info.local>
2025-03-13 17:00:48 -0300

    feat: adicionando pastas repository

---
+++ EventPlusAPI/Controllers/TipoEventoController.cs
@@ -0,0 +1,20 @@
+﻿using EventPlusAPI.Interfaces;
+using EventPlusAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlusAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TipoEventoController : ControllerBase
+    {
+        private readonly ITipoEventoRepository _tipoEventoRepository;
+
+        public TipoEventoController(ITipoEventoRepository tipoEventoRepository)
+        {
+            _tipoEventoRepository = tipoEventoRepository;
+        }
+    }
+}
---
+++ EventPlusAPI/Interfaces/IComentarioEventoRepository.cs
@@ -0,0 +1,16 @@
+﻿using EventPlusAPI.Domains;
+
+namespace EventPlusAPI.Interfaces
+{
+    public interface IComentarioEventoRepository
+    {
+        void Cadastrar(ComentarioEvento comentarioEvento);
+
+        void Deletar(Guid id);
+
+        List<ComentarioEvento> Listar(Guid id);
+
+        ComentarioEvento BuscarPorIdUsuario(Guid UsuarioID, Guid EventosID);
+
+    }
+}
